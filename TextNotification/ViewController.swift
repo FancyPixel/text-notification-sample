1cf73cc5a3e188b4319d0ec441955e27d81177b1
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-06-10 22:13:38 +0200

    Initial Commit

---
+++ TextNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextNotification
+//
+//  Created by Andrea Mazzini on 10/06/15.
+//  Copyright © 2015 Fancy Pixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
